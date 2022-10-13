c8e988fc0b893c8bce5c03aa80e1a81b1368944c
Jeremy Hernández Balderas <jeremy.hdez9@gmail.com>
2022-10-13 08:14:10 -0500

    Cambios menores a index por Jeremy

--- Nuevo exportado/JS/Redes_Sociales.js
+++ Nuevo exportado/JS/Redes_Sociales.js
@@ -1,19 +1,15 @@
 function AbrirFacebook() {
-    alert("Le redirigiré a mi perfil de Facebook...");
-    window.open('https://www.facebook.com/...', '_blank');
+  window.open("https://www.facebook.com/...", "_blank");
 }
 function AbrirWhatsApp() {
-    alert("Le redirigiré a un chat conmigo en WhatsApp...");
-    window.open('https://wa.me/...', '_blank');
+  window.open("https://wa.me/...", "_blank");
 }
 function AbrirMessenger() {
-    alert("Le redirigiré a un chat conmigo en Messenger...");
-    window.open('https://m.me/...', '_blank');
+  window.open("https://m.me/...", "_blank");
 }
 function AbrirTelegram() {
-    alert("Le redirigiré a un chat conmigo en Telegram...");
-    window.open('https://t.me/...', '_blank');
+  window.open("https://t.me/...", "_blank");
 }
 function AbrirTwitter() {
-    alert("Le redirigiré a mi cuenta de Twitter...");
-    window.open('https://www.twitter.com/...', '_blank');
+  window.open("https://www.twitter.com/...", "_blank");
+}
