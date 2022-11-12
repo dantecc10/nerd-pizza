4b573fb45d0828acdf971fcf8fb606f36867eea8
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-12 14:11:51 -0600

    quito un </div>

--- index.php
+++ index.php
@@ -91,6 +91,7 @@ $conexión = new mysqli("localhost", "nerdpizza", "nerdpizza!", "nerdpizza"); ?>
                                             </div>
                                         </div>
                                     </div>
+                                </div>
                                 </div>");
                                 $contador = ($contador + 1);
                             }
@@ -167,7 +168,6 @@ $conexión = new mysqli("localhost", "nerdpizza", "nerdpizza!", "nerdpizza"); ?>
                 #crearBanners($conexión, $consulta, $categoría, $resultado);
                 ?>
                 <!-- Terminan banners-->
-            </div>
             <div><a class="carousel-control-prev" href="#carousel-2" role="button" data-bs-slide="prev"><span class="carousel-control-prev-icon"></span><span class="visually-hidden">Previous</span></a><a class="carousel-control-next" href="#carousel-2" role="button" data-bs-slide="next"><span class="carousel-control-next-icon"></span><span class="visually-hidden">Next</span></a></div>
             <ol class="carousel-indicators">
                 <li data-bs-target="#carousel-2" data-bs-slide-to="0" class="active"></li>
