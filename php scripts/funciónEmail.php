3581ae5ef539f9cddc7ed44787420e26688d3993
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-12 18:55:48 -0600

    instrucciones de funciónEmail

--- php scripts/funciónEmail.php
+++ php scripts/funciónEmail.php
@@ -6,9 +6,17 @@ function enviarEmail($destinatario, $remitente, $asunto, $mensaje)
     mail($destinatario, $asunto, $mensaje, $origenDestino);
 }
 
-enviarEmail('jcmherrera@hotmail.com', 'nerdpizza@equipo1.prog5a.com', 'Esto deberia llegar de inmediato', 'Saludos desde un correo del servidor, licenciado Juan Carlos.');
-#$para = 'jeremy.hdez9@gmail.com';
-#$asunto = 'Prueba 1';
-#$mensaje = '¿Ves esto?';
-#mail($para, $asunto, $mensaje, $remitente);
-#echo $para . $asunto . $mensaje . $remitente;
+/*Esta línea manda llamar la función de enviar correos, y le pasa los parámetros necesarios para enviar el email. 
+#enviarEmail('jcmherrera@hotmail.com', 'nerdpizza@equipo1.prog5a.com', 'Esto deberia llegar de inmediato', 'Saludos desde un correo del servidor, licenciado Juan Carlos.');*/
+
+/*Pasos para usar esta función:
+    1.- Incluye esta función con include('funciónEmail');
+    2.- Prepara las siguientes variables en tu página PHP:
+        a)$destinatario
+        b)$remitente
+        c)$asunto
+        d)$mensaje
+    3.- Cuando tengas todo listo; envía el correo con la siguiente línea:
+        - enviarEmail($destinatario, $remitente, $asunto, $mensaje);
+    4-. Presúmelo a los demás ;-D
+*/
