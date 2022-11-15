1ba3140b2988a163e5c7be0cd4d4f1c331c115b1
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-15 07:42:15 -0600

    script que envía la clave de recuperación por correo

--- php scripts/crearClaveRecuperación.php
+++ php scripts/crearClaveRecuperación.php
@@ -1,4 +1,5 @@
 <?php
+include 'funciónEmail.php';
 $claveRecuperación = "";
 $claveRecuperaciónEmail = "";
 function generaClave($claveRecuperación, $claveRecuperaciónEmail)
@@ -17,12 +18,10 @@ function generaClave($claveRecuperación, $claveRecuperaciónEmail)
     }
     #return $claveRecuperación;
     echo $claveRecuperaciónEmail;
-    enviarEmail($destinatario, $remitente, $asunto, $mensaje)
-{
-    include('Mail.php');
-    $origenDestino = "From: $remitente" . " \r\n" . "CC: " . $destinatario;
-    mail($destinatario, $asunto, $mensaje, $origenDestino);
-}
+    $mensaje = ("Hola, somos del equipo de Nerd Pizza.
+    \nRecibimos una solicitud de reestablecimiento de tu contraseña, para eso, deberás ingresar la siguiente clave de recuperación en equipo.prog5a.com/Restablecer.php.
+    \nSi no has sido tú, ignora este mensaje.");
+    enviarEmail('dantecc10@gmail.com', 'nerdpizzza@equipo1.prog5a.com', 'Clave de recuperación', $mensaje);
 }
 function enviarEmail($destinatario, $remitente, $asunto, $mensaje)
 {
