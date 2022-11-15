8cfc8933dedc35b8163bc1613c459d43d75f9643
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-14 22:22:49 -0600

    no, pues no era el igual lo que faltaba

--- php scripts/claveRecuperacion.php
+++ php scripts/claveRecuperacion.php
@@ -3,7 +3,7 @@
         $characters = '0123456789';
         $charactersLength = strlen($characters);
         $randomString = '';
-        for ($i = 0; $i = $length; $i++) {
+        for ($i = 0; $i < $length; $i++) {
             $randomString .= $characters[rand(1, $charactersLength + 1)];
         }
         return $randomString;
