be834ff787079814eb7e65102d05e01e9d9dd2f8
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-14 22:52:47 -0600

    GENERACIÓN DE ALEATORIOS

---
+++ php scripts/crearClaveRecuperación.php
@@ -0,0 +1,23 @@
+<?php
+
+
+function generaClave()
+{
+    $contadorDígitos = 1;
+    $min = 0;
+    $max = 9;
+    $dígitoAleatorioGenerado = 0;
+    $claveRecuperación = "";
+    while ($contadorDígitos != 6) {
+        if ($contadorDígitos == 1) {
+            $dígitoAleatorioGenerado = rand(1, $max);
+        } else {
+            $dígitoAleatorioGenerado = rand($min, $max);
+        }
+
+        $claveRecuperación = ("" . $dígitoAleatorioGenerado);
+
+        $contadorDígitos++;
+    }
+    echo $claveRecuperación;
+}
