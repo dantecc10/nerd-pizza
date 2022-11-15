6f917f536c88d527bfd3cb18daa3173e471d5bff
Jeremy Hernández Balderas <jeremy.hdez9@gmail.com>
2022-11-14 19:47:08 -0600

    ..

--- php scripts/claveRecuperacion.php
+++ php scripts/claveRecuperacion.php
@@ -2,4 +2,16 @@
     srand(time());
     $clave= rand(1,6);
     echo $clave;
+
+    function generateRandomString($length = 10) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+    
+    echo generateRandomString();
 ?>
