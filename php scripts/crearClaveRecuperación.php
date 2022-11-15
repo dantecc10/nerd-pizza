18f6487f0b45cfcaca568e56b22edf093068a5fa
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-14 23:10:51 -0600

    dale

--- php scripts/crearClaveRecuperación.php
+++ php scripts/crearClaveRecuperación.php
@@ -13,7 +13,7 @@ function generaClave()
         #    $dígitoAleatorioGenerado = rand($min, $max);
         #}
         $dígitoAleatorioGenerado = rand($min, $max);
-        $claveRecuperación = ("" . "$dígitoAleatorioGenerado");
+        $claveRecuperación = ("$claveRecuperación" . "$dígitoAleatorioGenerado");
 
         $contadorDígitos++;
     }
