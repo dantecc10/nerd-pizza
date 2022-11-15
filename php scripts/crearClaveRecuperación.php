2a94f9a6c5be36a789033d341727ef8bd28146c5
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-14 23:13:20 -0600

    venga, funciona, funciona

--- php scripts/crearClaveRecuperación.php
+++ php scripts/crearClaveRecuperación.php
@@ -6,7 +6,11 @@ function generaClave()
     $max = 9;
     $dígitoAleatorioGenerado = 0;
     $claveRecuperación = "";
-    while ($contadorDígitos < 7) {
+
+
+    $dígitoAleatorioGenerado = rand(1, $max);
+    $claveRecuperación = ("$claveRecuperación" . "$dígitoAleatorioGenerado");
+    while ($contadorDígitos < 6) {
         #if ($contadorDígitos = 1) {
         #    $dígitoAleatorioGenerado = rand(1, $max);
         #} else {
