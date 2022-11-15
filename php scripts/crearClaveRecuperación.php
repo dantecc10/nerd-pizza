d9498726176148d2580fb5449171ac5def7d7e02
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-14 23:18:20 -0600

    comenta primera impresión

--- php scripts/crearClaveRecuperación.php
+++ php scripts/crearClaveRecuperación.php
@@ -5,17 +5,17 @@ function generaClave($claveRecuperación)
     $contadorDígitos = 0;
     $min = 0;
     $max = 9;
-    $dígitoAleatorioGenerado = 0;
-
     $dígitoAleatorioGenerado = rand(1, $max);
     $claveRecuperación = ("$claveRecuperación" . "$dígitoAleatorioGenerado");
+
     while ($contadorDígitos < 5) {
         $dígitoAleatorioGenerado = rand($min, $max);
         $claveRecuperación = ("$claveRecuperación" . "$dígitoAleatorioGenerado");
 
         $contadorDígitos++;
     }
-    echo $claveRecuperación;
+
+    #echo $claveRecuperación;
     return $claveRecuperación;
 }
 generaClave($claveRecuperación);
