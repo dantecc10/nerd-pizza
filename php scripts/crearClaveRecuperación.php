035a7c5e13763fac40be800d05ce3818cfe6f981
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-15 07:36:43 -0600

    echo boludo

--- php scripts/crearClaveRecuperación.php
+++ php scripts/crearClaveRecuperación.php
@@ -15,7 +15,14 @@ function generaClave($claveRecuperación, $claveRecuperaciónEmail)
 
         $contadorDígitos++;
     }
-    return $claveRecuperación;
-    #echo $claveRecuperaciónEmail;
+    #return $claveRecuperación;
+    echo $claveRecuperaciónEmail;
 }
+function enviarEmail($destinatario, $remitente, $asunto, $mensaje)
+{
+    include('Mail.php');
+    $origenDestino = "From: $remitente" . " \r\n" . "CC: " . $destinatario;
+    mail($destinatario, $asunto, $mensaje, $origenDestino);
+}
+
 generaClave($claveRecuperación, $claveRecuperaciónEmail);
