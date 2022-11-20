9fae350147cb18ecc61b3af273fc1dc1f9f5861f
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-19 21:10:02 -0600

    cierro div

--- ingredientes.php
+++ ingredientes.php
@@ -107,10 +107,10 @@
                     </div>
                 ");
                 }
+                echo "</div>";
             }
             generarCatálogo('pizzas', $conexión);
             ?>
-        </div>
         </div>
         <!-- Copia de tarjetas con buen formato en Bootstrap -->
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
