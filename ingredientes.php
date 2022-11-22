a575d1d5014af63a81da86b46647d7763be05e5c
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-22 15:31:49 -0600

    copy and page

--- ingredientes.php
+++ ingredientes.php
@@ -62,13 +62,14 @@
         </main>
         <!-- Copia de tarjetas de catálogo prueba-pagos -->
         <div class='container'>
+
             <?php
             $conexión = new mysqli("localhost", "nerdpizza", "nerdpizza!", "nerdpizza");
             function generarCatálogo($categoría, $conexión)
             {
                 echo "<div class='row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 align-items-center'>";
                 $contador = 1;
-                $consulta = "SELECT * FROM '$categoría'";
+                $consulta = "SELECT * FROM `$categoría`";
                 $resultado = mysqli_query($conexión, $consulta) or die("Error en la consulta a la base de datos");
                 while ($columna = mysqli_fetch_array($resultado)) {
                     $idPizza = $columna['idPizza'];
@@ -78,34 +79,33 @@
                     $fotoPizza = ("assets/img/pizzas/" . $idPizza . "/principal.png");
 
                     echo ("<div class='col align-middle'>
-                                <div class='card shadow-sm'>
-                                    <img src='$fotoPizza'>
-                                    <div class='card-body'>
-                                        <h5 class='card-title'>$nombrePizza</h5>
-                                        <p class='card-text'>Contiene: $ingredientes </p>
-                                        <div class='d-flex justify-content-between align-items-center'>
-                                            <div class='btn-group'>
-                                                <a href='details.php?id=1&amp;token=bf072c2eadbfadc7cd53cf14a205624f33357ac7' class='btn btn-primary detalles-card'>Detalles</a>
-                                                    <select name='tamaño' class='tamaño-card'>
-                                                        <option for='tamaño' value='1'>Chica</option>
-                                                        <option for='tamaño' value='2'>Mediana</option>
-                                                        <option for='tamaño' value='3'>Grande</option>
-                                                        <option for='tamaño' value='4'>Familiar</option>
-                                                        <option for='tamaño' value='5'>Jumbo</option>
-                                                    </select>
-                                                <input type='number' step='1' class='cantidad-card'>
-                                            </div>
-                                            <button class='btn btn-outline-success carrito-card' type='button' onclick='addProducto($idPizza, ' bf072c2eadbfadc7cd53cf14a205624f33357ac7')'>Agregar al carrito</button>
-                                        </div>
+                    <div class='card shadow-sm'>
+                        <img src='$fotoPizza'>
+                        <div class='card-body'>
+                            <h5 class='card-title'>$nombrePizza</h5>
+                            <p class='card-text'>Contiene: $ingredientes </p>
+                            <div class='d-flex justify-content-between align-items-center'>
+                                <div class='btn-group'>
+                                    <a href='details.php?id=1&amp;token=bf072c2eadbfadc7cd53cf14a205624f33357ac7' class='btn btn-primary detalles-card'>Detalles</a>
+                                    <select name='tamaño' class='tamaño-card'>
+                                        <option for='tamaño' value='1'>Chica</option>
+                                        <option for='tamaño' value='2'>Mediana</option>
+                                        <option for='tamaño' value='3'>Grande</option>
+                                        <option for='tamaño' value='4'>Familiar</option>
+                                        <option for='tamaño' value='5'>Jumbo</option>
+                                    </select>
+                                    <input type='number' step='1' class='cantidad-card'>
                                     </div>
+                                    <button class='btn btn-outline-success carrito-card' type='button' onclick='addProducto($idPizza, ' bf072c2eadbfadc7cd53cf14a205624f33357ac7')'>Agregar al carrito</button>
                                 </div>
-                            </div>");
+                            </div>
+                        </div>
+                    </div>
+                ");
                 }
                 echo "</div>";
             }
-            $categoría = 'pizzas';
             generarCatálogo('pizzas', $conexión);
-            #generarCatálogo('complementos', $conexión);
             ?>
         </div>
         <!-- Copia de tarjetas con buen formato en Bootstrap -->
