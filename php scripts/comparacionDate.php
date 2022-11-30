cc9f88bf166740e357f646bc97544f2d8978d5e6
Jeremy Hernández Balderas <jeremy.hdez9@gmail.com>
2022-11-30 11:19:55 -0600

    ..

--- php scripts/comparacionDate.php
+++ php scripts/comparacionDate.php
@@ -1,9 +1,9 @@
 <?php
     $date1 = new DateTime("25 Dec 2021");
     $date2 = new DateTime("now");
-/*
+
     $diffDays = $now->diff($next);
- 
+/* 
     echo $days_next->format('%a days');
    
     if ($date2 < $date1) {
