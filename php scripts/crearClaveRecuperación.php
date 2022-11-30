66fd66999e7987599f833d71752414ca90e9edaf
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-30 11:55:05 -0600

    algo sobre date

--- php scripts/crearClaveRecuperación.php
+++ php scripts/crearClaveRecuperación.php
@@ -25,21 +25,21 @@ $claveRecuperaciónEmail = generaClave($claveRecuperación);
 
 function cargarClaveRecuperación($claveRecuperaciónEmail, $éxitoCarga)
 {
-
-    $destinatario = '';
-    $claveRecuperaciónEmail;
-    $usadaClave = false;
+    $destinatario = $_POST['CorreoRecuperación'];
     $pruebaClave = true;
-    $emisiónClave = "";
+
+    $emisiónClave = new DateTime('now');
 
     $conexión = new mysqli("localhost", "nerdpizza", "nerdpizza!", "nerdpizza");
     $consulta = "SELECT * FROM `claves`";
     $resultado = mysqli_query($conexión, $consulta) or die("Error en la consulta a la base de datos");
     while ($columna = mysqli_fetch_array($resultado)) {
         if ($columna['clave_recuperación'] == $claveRecuperaciónEmail) {
+            $usadaClave = true;
             break;
             $éxitoCarga = false;
         } else {
+            $usadaClave = false;
             $consulta = "INSERT INTO `claves` (`id_clave`, `correo_clave`, `clave_recuperación`, `emisión_clave`, `usada_clave`, `prueba_clave`) VALUES ('', '$destinatario', '$claveRecuperaciónEmail', '$emisiónClave', $usadaClave, $pruebaClave)";
             $resultado = mysqli_query($conexión, $consulta) or die("Error en la consulta a la base de datos");
             $éxitoCarga = true;
