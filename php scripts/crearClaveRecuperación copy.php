1c64911156ae44902c6462102a2efa5ac3b8747d
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-30 10:46:37 -0600

    copia

--- php scripts/crearClaveRecuperación copy.php
+++ php scripts/crearClaveRecuperación copy.php
@@ -1,34 +1,37 @@
 <?php
-include 'funciónEmail.php';
-$claveRecuperación = "";
-$claveRecuperaciónEmail = "";
+include "funciónEmail.php";
+$claveRecuperación = 0;
+$claveRecuperaciónEmail;
 function generaClave($claveRecuperación, $claveRecuperaciónEmail)
 {
     $contadorDígitos = 0;
     $min = 0;
     $max = 9;
     $dígitoAleatorioGenerado = rand(1, $max);
-    $claveRecuperación = ("$claveRecuperación" . "$dígitoAleatorioGenerado");
+    $claveRecuperación = ($claveRecuperación . $dígitoAleatorioGenerado);
 
     while ($contadorDígitos < 5) {
         $dígitoAleatorioGenerado = rand($min, $max);
-        $claveRecuperación = ("$claveRecuperación" . "$dígitoAleatorioGenerado");
+        $claveRecuperación = ($claveRecuperación . $dígitoAleatorioGenerado);
 
         $contadorDígitos++;
     }
-    #return $claveRecuperación;
-    echo $claveRecuperaciónEmail;
-    $mensaje = ("Hola, somos del equipo de Nerd Pizza.
-    /nRecibimos una solicitud de reestablecimiento de tu contraseña, para eso, deberás ingresar la siguiente clave de recuperación en equipo.prog5a.com/Restablecer.php.
-    /nSi no has sido tú, ignora este mensaje.");
-    enviarEmail('dantecc10@gmail.com', 'nerdpizzza@equipo1.prog5a.com', 'Clave de recuperación', $mensaje);
+
+    echo $claveRecuperación;
+    return $claveRecuperación;
 }
+
+echo "Aquí se va a enviar el email";
+enviarEmail('dante@castelancarpinteyro.club', 'nerdpizza@equipo1.prog5a.com', 'Clave de recuperación', ("Su clave de recuperación es " . generaClave($claveRecuperación, $claveRecuperaciónEmail)));
+echo "Aquí ya se debió enviar el email";
+
+/* 
+
 function enviarEmail($destinatario, $remitente, $asunto, $mensaje)
 {
     include('Mail.php');
     $origenDestino = "From: $remitente" . " /r/n" . "CC: " . $destinatario;
     mail($destinatario, $asunto, $mensaje, $origenDestino);
 }
-
-
 generaClave($claveRecuperación, $claveRecuperaciónEmail);
+?> */
