0b1862dcfab45c64b89f89ef7e37c1f5e866afb8
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-30 23:50:56 -0600

    validación de usuario añadida

--- php scripts/crearClaveRecuperación.php
+++ php scripts/crearClaveRecuperación.php
@@ -2,6 +2,12 @@
 
 function madreFunciones()
 {
+    $conexión = new mysqli("localhost", "nerdpizza", "nerdpizza!", "nerdpizza");
+    $correoRecuperación = $_POST['correoRecuperación'];
+
+    $consulta = "SELECT count(*) FROM `usuarios` WHERE `emailU` = '$correoRecuperación'";
+    $existenciaUsuario = mysqli_query($conexión, $consulta) or die("Error en la consulta a la base de datos");
+
     # Generación de clave
     function generaClave()
     {
@@ -15,40 +21,42 @@ function madreFunciones()
         return $claveRecuperación;
     }
 
-    $claveRecuperaciónEmail = generaClave();
-    echo ("Aquí está la clave de recuperación heredada de la función generadora: " . $claveRecuperaciónEmail);
+    if ($existenciaUsuario == true) {
 
-    # Carga a base de datos
-    $conexión = new mysqli("localhost", "nerdpizza", "nerdpizza!", "nerdpizza");
-    $consulta = "SELECT count(*) FROM `claves` WHERE `clave_recuperación`=$claveRecuperaciónEmail";
+        $claveRecuperaciónEmail = generaClave();
+        echo ("Aquí está la clave de recuperación heredada de la función generadora: " . $claveRecuperaciónEmail);
 
-    $resultado = mysqli_query($conexión, $consulta) or die("Error en la consulta a la base de datos");
+        # Carga a base de datos
+        $consulta = "SELECT count(*) FROM `claves` WHERE `clave_recuperación`=$claveRecuperaciónEmail";
+        $resultado = mysqli_query($conexión, $consulta) or die("Error en la consulta a la base de datos");
 
-    if ($resultado == true) {
-        # Envío de email y clave validada
-        echo "Validación exitosa";
+        if ($resultado == true) {
+            # Envío de email y clave validada
+            echo "Validación exitosa";
 
-        $correoRecuperación = $_POST['correoRecuperación'];
-        echo $correoRecuperación;
+            echo $correoRecuperación;
 
-        $consulta = "INSERT INTO `claves`
+            $consulta = "INSERT INTO `claves`
         (`id_clave`, `correo_clave`, `clave_recuperación`, `emisión_clave`, `usada_clave`, `prueba_clave`)
         VALUES ('','$correoRecuperación','$claveRecuperaciónEmail','2022-11-30','0','1')";
-        $inserción = mysqli_query($conexión, $consulta) or die("Error en la consulta a la base de datos");
+            $inserción = mysqli_query($conexión, $consulta) or die("Error en la consulta a la base de datos");
 
-        # Sección de email
-        include('Mail.php');
-        $mensaje = ("Nerd Pizza ha recibido una solicitud de reestablecimiento de contraseña; ingresa el siguiente código para poder cambiar tu contraseña: " . $claveRecuperaciónEmail . ".");
-        $remitente = "nerdpizza@equipo1.prog5a.com";
-        $destinatario = $correoRecuperación;
-        $asunto = "Reestablecimiento de contraseña";
+            # Sección de email
+            include('Mail.php');
+            $mensaje = ("Nerd Pizza ha recibido una solicitud de reestablecimiento de contraseña; ingresa el siguiente código para poder cambiar tu contraseña: " . $claveRecuperaciónEmail . ".");
+            $remitente = "nerdpizza@equipo1.prog5a.com";
+            $destinatario = $correoRecuperación;
+            $asunto = "Reestablecimiento de contraseña";
 
-        $origenDestino = "From: $remitente" . " /r/n" . "CC: " . $destinatario;
-        mail($destinatario, $asunto, $mensaje, $origenDestino);
+            $origenDestino = "From: $remitente" . " /r/n" . "CC: " . $destinatario;
+            mail($destinatario, $asunto, $mensaje, $origenDestino);
 
-        echo ("Aquí ya se envió la clave... se supone");
+            echo ("Aquí ya se envió la clave... se supone");
+        } else {
+            echo "La clave ya existe o no pudo ser validad en la base de datos.";
+        }
     } else {
-        echo "La clave ya existe o no pudo ser validad en la base de datos.";
+        echo "El correo ingresado no está asociado a ninguna cuenta de Nerd Pizza, verifica el correo o crea una cuenta...";
     }
 }
 madreFunciones();
