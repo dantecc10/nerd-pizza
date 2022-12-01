78336a4933d77ff2b7218df1bae3583e8a38473e
Jeremy Hernández Balderas <jeremy.hdez9@gmail.com>
2022-11-30 21:38:37 -0600

    ..

--- php scripts/registroU.php
+++ php scripts/registroU.php
@@ -2,7 +2,7 @@
 
     $conexión = new mysqli("localhost", "nerdpizza", "nerdpizza!", "nerdpizza");
     $basededatos = "nerdpizza";
-    $db = mysqli_select_db($conexiòn,$basededatos)
+    $db = mysqli_select_db($conexiòn,$basededatos);
     
     if (isset($_POST["nombre"], $_POST["apellido"], $_POST["email"], $_POST["pass"]) and $_POST["nombre"] !="" and $_POST["apellido"]!="" and $_POST["email"]!="" and $_POST["nombre"] !=""){
             $nombre = $_POST["nombre"];
