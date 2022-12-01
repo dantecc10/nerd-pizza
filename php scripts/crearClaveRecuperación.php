fbd92a9277635d8aa14c47a8033c6d1a184c9251
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-30 23:11:06 -0600

    avances para validación

--- php scripts/crearClaveRecuperación.php
+++ php scripts/crearClaveRecuperación.php
@@ -19,6 +19,37 @@ function madreFunciones()
     echo ("Aquí está la clave de recuperación heredada de la función generadora: " . $claveRecuperaciónEmail);
 
     # Carga a base de datos
+    $conexión = new mysqli("localhost", "nerdpizza", "nerdpizza!", "nerdpizza");
+    $consulta = "SELECT count(*) FROM `claves` WHERE `clave_recuperación`=$claveRecuperaciónEmail";
+
+    $resultado = mysqli_query($conexión, $consulta) or die("Error en la consulta a la base de datos");
+
+    if ($resultado != 0) {
+        echo "Algo anda mal en la comparación de claves o resultó ser 123456";
+    } else {
+        echo "Validación exitosa";
+    }
+
+    echo ('<tr>');
+    echo ('<th>`id_clave`</th>');
+    echo ('<th>`correo_clave`</th>');
+    echo ('<th>`clave_recuperación`</th>');
+    echo ('<th>`emisión_clave`</th>');
+    echo ('<th>`usada_clave`</th>');
+    echo ('<th>`prueba_clave`</th>');
+    echo ('</tr>');
+    while ($columna = mysqli_fetch_array($resultado)) {
+        echo ("<tr>");
+
+        echo ("<td>" . $columna['id_clave'] . "</td>");
+        echo ("<td>" . $columna['correo_clave'] . "</td>");
+        echo ("<td>" . $columna['clave_recuperación'] . "</td>");
+        echo ("<td>" . $columna['emisión_clave'] . "</td>");
+        echo ("<td>" . $columna['usada_clave'] . "</td>");
+        echo ("<td>" . $columna['prueba_clave'] . "</td>");
+
+        echo ("</tr>");
+    }
 
     # Envío de email y clave validada
     # Sección de email
