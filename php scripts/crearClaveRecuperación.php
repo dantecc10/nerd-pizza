778267cb89a6b0860a4128f5e4fb0088c80e56ce
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-12-01 11:06:28 -0600

    dante

--- php scripts/crearClaveRecuperación.php
+++ php scripts/crearClaveRecuperación.php
@@ -22,6 +22,39 @@ function madreFunciones()
     $noExistenciaUsuario = mysqli_query($conexión, $consulta) or die("Error en la consulta a la base de datos");
 
 
+    $claveRecuperaciónEmail = generaClave();
+    echo ("Aquí está la clave de recuperación heredada de la función generadora: " . $claveRecuperaciónEmail);
+
+    # Carga a base de datos
+    $consulta = "SELECT count(*) FROM `claves` WHERE `clave_recuperación`=$claveRecuperaciónEmail";
+    $resultado = mysqli_query($conexión, $consulta) or die("Error en la consulta a la base de datos");
+
+    if ($resultado == true) {
+        # Envío de email y clave validada
+        echo "Validación exitosa";
+
+        echo $correoRecuperación;
+
+        $consulta = "INSERT INTO `claves`
+        (`id_clave`, `correo_clave`, `clave_recuperación`, `emisión_clave`, `usada_clave`, `prueba_clave`)
+        VALUES ('','$correoRecuperación','$claveRecuperaciónEmail','2022-11-30','0','1')";
+        $inserción = mysqli_query($conexión, $consulta) or die("Error en la consulta a la base de datos");
+
+        # Sección de email
+        include('Mail.php');
+        $mensaje = ("Nerd Pizza ha recibido una solicitud de reestablecimiento de contraseña; ingresa el siguiente código para poder cambiar tu contraseña: " . $claveRecuperaciónEmail . ".");
+        $remitente = "nerdpizza@equipo1.prog5a.com";
+        $destinatario = $correoRecuperación;
+        $asunto = "Reestablecimiento de contraseña";
+
+        $origenDestino = "From: $remitente" . " /r/n" . "CC: " . $destinatario;
+        mail($destinatario, $asunto, $mensaje, $origenDestino);
+
+        echo ("Aquí ya se envió la clave... se supone");
+    } else {
+        echo "La clave ya existe o no pudo ser validad en la base de datos.";
+    }
+    /*
     if ($noExistenciaUsuario == false) {
 
         $claveRecuperaciónEmail = generaClave();
@@ -58,6 +91,6 @@ function madreFunciones()
         }
     } else {
         echo "El correo ingresado no está asociado a ninguna cuenta de Nerd Pizza, verifica el correo o crea una cuenta...";
-    }
+    }*/
 }
 madreFunciones();
