5b76a0d9c5c583fdaf0fb886661e263c88b56aed
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-30 23:24:39 -0600

    una prueba de inserción a la base de datos

--- php scripts/crearClaveRecuperación.php
+++ php scripts/crearClaveRecuperación.php
@@ -25,14 +25,21 @@ function madreFunciones()
     $resultado = mysqli_query($conexión, $consulta) or die("Error en la consulta a la base de datos");
 
     if ($resultado == true) {
+        # Envío de email y clave validada
         echo "Validación exitosa";
 
-        # Envío de email y clave validada
+        $correoRecuperación = "dantecc10@gmail.com";
+
+        $consulta = "INSERT INTO `claves`
+        (`id_clave`, `correo_clave`, `clave_recuperación`, `emisión_clave`, `usada_clave`, `prueba_clave`)
+        VALUES ('','$correoRecuperación','$claveRecuperaciónEmail','2022-11-30','false','true')";
+        $inserción = mysqli_query($conexión, $consulta) or die("Error en la consulta a la base de datos");
+
         # Sección de email
         include('Mail.php');
         $mensaje = ("Nerd Pizza ha recibido una solicitud de reestablecimiento de contraseña; ingresa el siguiente código para poder cambiar tu contraseña: " . $claveRecuperaciónEmail . ".");
         $remitente = "nerdpizza@equipo1.prog5a.com";
-        $destinatario = "dante@castelancarpinteyro.club, jeremy.hdez9@gmail.com";
+        $destinatario = "dante@castelancarpinteyro.club";
         $asunto = "Reestablecimiento de contraseña";
 
         $origenDestino = "From: $remitente" . " /r/n" . "CC: " . $destinatario;
