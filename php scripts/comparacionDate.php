873f0ef0bcd3f69a9f447fd653e122c969797e2b
Jeremy Hernández Balderas <jeremy.hdez9@gmail.com>
2022-12-02 08:52:53 -0600

    segun la fecha

--- php scripts/comparacionDate.php
+++ php scripts/comparacionDate.php
@@ -1,12 +1,5 @@
 <?php
-    $date1 = new DateTime("25 Dec 2022");
-    $now = new DateTime("now");
-
-    $diffDays = $now->diff($date1);
-
-    echo $diffDays->format('%a days');
-
-/*     if ($now > $date1) {
-        echo "aun no es navidad";
-    } */
+    $fechaActual = date('d/m/y h:i:s');
+    echo "segun es la fecha actual" . "<br>";
+    echo $fechaActual;
 ?>
