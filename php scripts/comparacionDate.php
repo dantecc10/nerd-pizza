972f61d8a12dd59a963b4eec02f4cb5b1e6ff718
Jeremy Hernández Balderas <jeremy.hdez9@gmail.com>
2022-12-02 09:06:43 -0600

    ..

--- php scripts/comparacionDate.php
+++ php scripts/comparacionDate.php
@@ -1,7 +1,12 @@
 <?php
     date_default_timezone_set('America/Mexico_City');
 
+    $navida = date('22,12,25');
     $fechaActual = date('y/m/d h:i:s');
     echo "segun es la fecha actual" . "<br>";
     echo $fechaActual;
+
+    if ($fechaActual < $navidad) {
+        echo "aun no es navidad";
+    }
 ?>
