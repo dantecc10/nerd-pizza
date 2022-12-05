34b77cdcec943bd705a27933c28b099b842ee99b
Jeremy Hernández Balderas <jeremy.hdez9@gmail.com>
2022-12-04 22:21:08 -0600

    ..

--- index.php
+++ index.php
@@ -27,6 +27,7 @@ include "php scripts/InicioSesión.php";
 </head>
 
 <body class="text-body" style="background-color: #969A97;">
+    <!-- Login -->
     <div id="DivInicioSesión" class="modal">
         <form class="modal-content animate myForm" action="" method="post">
             <div class="imgcontainer">
@@ -62,7 +63,7 @@ include "php scripts/InicioSesión.php";
         <!-- Header -->
         <nav class="navbar navbar-light navbar-expand-md py-3">
             <div class="container"><a class="navbar-brand d-flex align-items-center" href="#"><span class="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center bs-icon me-2">
-                        <img src="assets/img/Nerd-Pizza.png" alt="Nerd Pizza" id="LogoNerdPizza"> </span>
+                    <img src="assets/img/Nerd-Pizza.png" alt="Nerd Pizza" id="LogoNerdPizza"> </span>
                     <path fill-rule="evenodd" d="M0 10.5A1.5 1.5 0 0 1 1.5 9h1A1.5 1.5 0 0 1 4 10.5v1A1.5 1.5 0 0 1 2.5 13h-1A1.5 1.5 0 0 1 0 11.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm10.5.5A1.5 1.5 0 0 1 13.5 9h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM6 4.5A1.5 1.5 0 0 1 7.5 3h1A1.5 1.5 0 0 1 10 4.5v1A1.5 1.5 0 0 1 8.5 7h-1A1.5 1.5 0 0 1 6 5.5v-1zM7.5 4a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z">
                     </path>
                     <path d="M6 4.5H1.866a1 1 0 1 0 0 1h2.668A6.517 6.517 0 0 0 1.814 9H2.5c.123 0 .244.015.358.043a5.517 5.517 0 0 1 3.185-3.185A1.503 1.503 0 0 1 6 5.5v-1zm3.957 1.358A1.5 1.5 0 0 0 10 5.5v-1h4.134a1 1 0 1 1 0 1h-2.668a6.517 6.517 0 0 1 2.72 3.5H13.5c-.123 0-.243.015-.358.043a5.517 5.517 0 0 0-3.185-3.185z">
@@ -76,7 +77,7 @@ include "php scripts/InicioSesión.php";
                         <li class="nav-item"></li>
                         <li class="nav-item"></li>
                         <li class="nav-item">
-                            <a class="nav-link" onclick="document.getElementById('DivInicioSesión').style.display='absolute'"> <i class="far fa-user" style="font-size: 18px;"></i>&nbsp;Iniciar sesión</a>
+                            <a class="nav-link" onclick="document.getElementById('DivInicioSesión').style.display='absolute'" href="#"> <i class="far fa-user" style="font-size: 18px;"></i>&nbsp;Iniciar sesión</a>
                         </li>
                     </ul>
                     <div class="dropdown show">
