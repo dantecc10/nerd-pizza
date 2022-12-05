6910506ca87fe00e2a37db7175a38830f4c5cd3f
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-12-05 07:52:19 -0600

    probando función de script para mostrar modal

---
+++ assets/js/ModalInicioSesión.js
@@ -0,0 +1,6 @@
+function mostrarLogin() {
+    document.getElementById('DivInicioSesión').style.display = 'absolute';
+}
+function ocultarLogin() {
+    document.getElementById('DivInicioSesión').style.display = 'none';
+}
