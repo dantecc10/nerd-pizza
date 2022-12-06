f91e008fd0c042d6ce94cc6ac25ca46adb2b8345
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-12-05 19:05:42 -0600

    OTRO PESO DE PHP

--- formcorreo.php
+++ formcorreo.php
@@ -1,32 +1,39 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <form method="post" action="php scripts/EnvioRecibo.php">
         <fieldset>
-        <legend> Ingrese su consulta</legend>
-        <p>
-            <label> Escriba su correo:
-            <input type="text" name="email">
-            </label>
-        </p>
-        <p>
-            <label> Escriba su Nombre:
-            <input type="text" name="nombre">
-            </label>
-        </p>
-        <p>
-            <label>Escriba un mensaje:
-            <input type="text" name="mensaje">
-            </label>
-        </p>
-        <input type="submit" value="enviar">
+            <legend> Ingrese su consulta</legend>
+            <p>
+                <label> Escriba su correo:
+                    <input type="text" name="email">
+                </label>
+            </p>
+            <p>
+                <label> Escriba su Nombre:
+                    <input type="text" name="nombre">
+                </label>
+            </p>
+            <p>
+                <label>Escriba un mensaje:
+                    <input type="text" name="mensaje">
+                </label>
+            </p>
+            <input type="submit" value="enviar">
         </fieldset>
     </form>
+    <?php
+    #un pesito de PHP
+
+    ?>
 </body>
+
 </html>
