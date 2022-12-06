3517d98e564487a0a45f064ef93e45d7144b1332
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-12-05 23:16:03 -0600

    tu conexión toda rara, puras blasfemias

--- php scripts/registroU.php
+++ php scripts/registroU.php
@@ -1,19 +1,16 @@
 <?php
 
-    $conexión = new mysqli("localhost", "nerdpizza", "nerdpizza!", "nerdpizza");
-    $basededatos = "nerdpizza";
-    $db = mysqli_select_db($conexión,$basededatos) or 
-    die("No se ha podido conectar al servidor de Base de Datos");
-    
-    if (isset($_POST["nombre"], $_POST["apellido"], $_POST["email"], $_POST["pass"], $_POST["direccion"]) and $_POST["nombre"] !="" and $_POST["apellido"]!="" and $_POST["email"]!="" and $_POST["pass"] !="" and $_POST["direccion"] !=""){
-            $nombre = $_POST["nombre"];
-            $apellido = $_POST["apellido"];
-            $email = $_POST["email"];
-            $pass = $_POST["pass"];
-            $direccion = $_POST["direccion"];
-            $insert = "INSERT INTO usuarios VALUES ('','$nombre','$apellido','$email','$pass','$direccion');";
-        } 
-    else{
-        echo '<p>Por favor, complete el <a href="NERD-PIZZA/Registro.php">Registro</a></p>';
-    }
-?>
+$conexión = new mysqli("localhost", "nerdpizza", "nerdpizza!", "nerdpizza");
+$basededatos = "nerdpizza";
+$db = mysqli_select_db($conexión, $basededatos) or die("No se ha podido conectar al servidor de Base de Datos");
+
+if (isset($_POST["nombre"], $_POST["apellido"], $_POST["email"], $_POST["pass"], $_POST["direccion"]) and $_POST["nombre"] != "" and $_POST["apellido"] != "" and $_POST["email"] != "" and $_POST["pass"] != "" and $_POST["direccion"] != "") {
+    $nombre = $_POST["nombre"];
+    $apellido = $_POST["apellido"];
+    $email = $_POST["email"];
+    $pass = $_POST["pass"];
+    $direccion = $_POST["direccion"];
+    $insert = "INSERT INTO usuarios VALUES ('','$nombre','$apellido','$email','$pass','$direccion');";
+} else {
+    echo '<p>Por favor, complete el <a href="NERD-PIZZA/Registro.php">Registro</a></p>';
+}
