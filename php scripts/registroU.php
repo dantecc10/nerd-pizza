64666282de969ce1c7d500e3f4471411a75e0af3
Jeremy Hernández Balderas <jeremy.hdez9@gmail.com>
2022-12-06 18:13:44 -0600

    ..

--- php scripts/registroU.php
+++ php scripts/registroU.php
@@ -5,7 +5,7 @@
     $db = mysqli_select_db($conexión,$basededatos) or 
     die("No se ha podido conectar al servidor de Base de Datos");
     
-    if (!empty($_POST['formRegistro'])){
+    if (isset($_POST["nombre"], $_POST["apellido"], $_POST["email"], $_POST["pass"], $_POST["direccion"]) and $_POST["nombre"] !="" and $_POST["apellido"]!="" and $_POST["email"]!="" and $_POST["pass"] !="" and $_POST["direccion"] !=""){
             $nombre = $_POST["nombre"];
             $apellido = $_POST["apellido"];
             $email = $_POST["email"];
