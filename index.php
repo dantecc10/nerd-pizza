c1f410ff663aac76f8908e5d251a45e0e3f34f24
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-12-06 18:28:47 -0600

    soy un genio

--- index.php
+++ index.php
@@ -47,8 +47,12 @@ $conexión = new mysqli("localhost", "nerdpizza", "nerdpizza!", "nerdpizza");
                         <li class="nav-item"></li>
                         <li class="nav-item">
                             <a class="nav-link" onclick="document.getElementById('DivInicioSesión').style.display='block'"> <i class="far fa-user" style="font-size: 18px;"></i>
-                                &nbsp;                              <?php
-                                if ($_SESSION['idUsuario'] != "") {echo $_SESSION['nombreU'];} else {echo "Iniciar sesión";}?>Iniciar sesión
+                                &nbsp; <?php
+                                        if ($_SESSION['idUsuario'] != "") {
+                                            echo $_SESSION['nombreU'];
+                                        } else {
+                                            echo "Iniciar sesión";
+                                        } ?>
                             </a>
                         </li>
                     </ul>
