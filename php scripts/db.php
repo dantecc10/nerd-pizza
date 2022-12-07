31b760c13f82b7e2313928088b05221d60f15f90
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-12-06 18:24:11 -0600

    halo

--- php scripts/db.php
+++ php scripts/db.php
@@ -21,3 +21,57 @@ $conexión = new mysqli("localhost", "nerdpizza", "nerdpizza!", "nerdpizza"); ?>
     }
     ?>
 </table>
+
+<table class="InsertarResultados Examen">
+    <?php
+    $consulta = "SELECT * FROM `claves`";
+    $resultado = mysqli_query($conexión, $consulta) or die("Error en la consulta a la base de datos");
+    echo ('<tr>');
+    echo ('<th>`id_clave`</th>');
+    echo ('<th>`correo_clave`</th>');
+    echo ('<th>`clave_recuperación`</th>');
+    echo ('<th>`emisión_clave`</th>');
+    echo ('<th>`usada_clave`</th>');
+    echo ('<th>`prueba_clave`</th>');
+    echo ('</tr>');
+    while ($columna = mysqli_fetch_array($resultado)) {
+        echo ("<tr>");
+
+        echo ("<td>" . $columna['id_clave'] . "</td>");
+        echo ("<td>" . $columna['correo_clave'] . "</td>");
+        echo ("<td>" . $columna['clave_recuperación'] . "</td>");
+        echo ("<td>" . $columna['emisión_clave'] . "</td>");
+        echo ("<td>" . $columna['usada_clave'] . "</td>");
+        echo ("<td>" . $columna['prueba_clave'] . "</td>");
+
+        echo ("</tr>");
+    }
+    ?>
+</table>
+
+<table class="InsertarResultados Examen">
+    <?php
+    $consulta = "SELECT * FROM `usuarios`";
+    $resultado = mysqli_query($conexión, $consulta) or die("Error en la consulta a la base de datos");
+    echo ('<tr>');
+    echo ('<th>`id_clave`</th>');
+    echo ('<th>`correo_clave`</th>');
+    echo ('<th>`clave_recuperación`</th>');
+    echo ('<th>`emisión_clave`</th>');
+    echo ('<th>`usada_clave`</th>');
+    echo ('<th>`prueba_clave`</th>');
+    echo ('</tr>');
+    while ($columna = mysqli_fetch_array($resultado)) {
+        echo ("<tr>");
+
+        echo ("<td>" . $columna['id_clave'] . "</td>");
+        echo ("<td>" . $columna['correo_clave'] . "</td>");
+        echo ("<td>" . $columna['clave_recuperación'] . "</td>");
+        echo ("<td>" . $columna['emisión_clave'] . "</td>");
+        echo ("<td>" . $columna['usada_clave'] . "</td>");
+        echo ("<td>" . $columna['prueba_clave'] . "</td>");
+
+        echo ("</tr>");
+    }
+    ?>
+</table>
