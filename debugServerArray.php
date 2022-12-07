286c66f61269cd05d0ec20d5160e0f308def27cf
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-12-07 16:48:59 -0600

    prueba de cookie

--- debugServerArray.php
+++ debugServerArray.php
@@ -1,17 +1,22 @@
 <?php
-    echo ($_SERVER['PHP_SELF'] . "<br>");
-    echo ($_SERVER['SERVER_ADDR'] . "<br>");
-    echo ($_SERVER['SERVER_NAME'] . "<br>");
-    echo ($_SERVER['SERVER_SOFTWARE'] . "<br>");
-    echo ($_SERVER['SERVER_PROTOCOL'] . "<br>");
-    echo ($_SERVER['REQUEST_METHOD'] . "<br>");
-    echo ($_SERVER['DOCUMENT_ROOT'] . "<br>");
-    echo ($_SERVER['REMOTE_ADDR'] . "<br>");
-    echo ($_SERVER['REMOTE_HOST'] . "<br>");
-    echo ($_SERVER['REDIRECT_REMOTE_USER'] . "<br>");
-    echo ($_SERVER['SCRIPT_FILENAME'] . "<br>");
-    echo ($_SERVER[''] . "<br>");
-    echo ($_SERVER[''] . "<br>");
-    echo ($_SERVER[''] . "<br>");
-    echo ($_SERVER[''] . "<br>");
-    echo ($_SERVER[''] . "<br>");
+setcookie("Sesión", "Iniciada", time() + 24 * 3600, "/", "equipo1.prog5a.com");
+
+echo ($_COOKIE['Sesión'] . "<br>");
+
+
+echo ($_SERVER['PHP_SELF'] . "<br>");
+echo ($_SERVER['SERVER_ADDR'] . "<br>");
+echo ($_SERVER['SERVER_NAME'] . "<br>");
+echo ($_SERVER['SERVER_SOFTWARE'] . "<br>");
+echo ($_SERVER['SERVER_PROTOCOL'] . "<br>");
+echo ($_SERVER['REQUEST_METHOD'] . "<br>");
+echo ($_SERVER['DOCUMENT_ROOT'] . "<br>");
+echo ($_SERVER['REMOTE_ADDR'] . "<br>");
+echo ($_SERVER['REMOTE_HOST'] . "<br>");
+echo ($_SERVER['REDIRECT_REMOTE_USER'] . "<br>");
+echo ($_SERVER['SCRIPT_FILENAME'] . "<br>");
+echo ($_SERVER[''] . "<br>");
+echo ($_SERVER[''] . "<br>");
+echo ($_SERVER[''] . "<br>");
+echo ($_SERVER[''] . "<br>");
+echo ($_SERVER[''] . "<br>");
