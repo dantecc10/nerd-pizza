d39c8aa360d528ee69bbc43ca0d0c6c9e6c14b13
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-12-07 17:21:18 -0600

    uso de links src de css js assets con url absoluta

--- ingredientesRespaldo.php
+++ ingredientesRespaldo.php
@@ -17,13 +17,13 @@
 <html lang="es">
 
 <head>
-    <link rel="icon" type="image/jpeg" sizes="960x957" href="assets\img\Nerd-Pizza.png">
+    <link rel="icon" type="image/jpeg" sizes="960x957" href="https://equipo1.prog5a.com/assets\img\Nerd-Pizza.png">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Ingredientes</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/Articles-Badges-images.css">
-    <link rel="stylesheet" href="assets/css/Navbar-Centered-Links-icons.css">
+    <link rel="stylesheet" href="https://equipo1.prog5a.com/assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://equipo1.prog5a.com/assets/css/Articles-Badges-images.css">
+    <link rel="stylesheet" href="https://equipo1.prog5a.com/assets/css/Navbar-Centered-Links-icons.css">
 </head>
 
 <body class="text-body" style="background-color: #969A97;">
@@ -32,7 +32,7 @@
         <nav class="navbar navbar-light navbar-expand-md py-3">
             <div class="container"><a class="navbar-brand d-flex align-items-center" href="https://equipo1.prog5a.com">
                     <span class="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center me-2 bs-icon">
-                        <img src="assets\img\Nerd-Pizza.png" alt="Nerd Pizza" id="LogoNerdPizza">
+                        <img src="https://equipo1.prog5a.com/assets\img\Nerd-Pizza.png" alt="Nerd Pizza" id="LogoNerdPizza">
                     </span>
                     <path fill-rule="evenodd" d="M0 10.5A1.5 1.5 0 0 1 1.5 9h1A1.5 1.5 0 0 1 4 10.5v1A1.5 1.5 0 0 1 2.5 13h-1A1.5 1.5 0 0 1 0 11.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm10.5.5A1.5 1.5 0 0 1 13.5 9h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM6 4.5A1.5 1.5 0 0 1 7.5 3h1A1.5 1.5 0 0 1 10 4.5v1A1.5 1.5 0 0 1 8.5 7h-1A1.5 1.5 0 0 1 6 5.5v-1zM7.5 4a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z">
                     </path>
@@ -78,7 +78,7 @@
                     $nombrePizza = $columna['nombrePizza'];
                     #$precioPizza = $columna['precioBebida'];
                     $ingredientes = $columna['ingredientes'];
-                    $fotoPizza = ("assets/img/pizzas/" . $idPizza . "/principal.png");
+                    $fotoPizza = ("https://equipo1.prog5a.com/assets/img/pizzas/" . $idPizza . "/principal.png");
 
                     echo ("<div class='col align-middle'>
                     <div class='card shadow-sm'>
@@ -116,7 +116,7 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <!--<div class="card-deck">
             <div class="card">
-                <img class="card-img-top" src="assets/img/pizzas/1/principal.png" alt="Card image cap">
+                <img class="card-img-top" src="https://equipo1.prog5a.com/assets/img/pizzas/1/principal.png" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -124,7 +124,7 @@
                 </div>
             </div>
             <div class="card">
-                <img class="card-img-top" src="assets/img/pizzas/1/principal.png" alt="Card image cap">
+                <img class="card-img-top" src="https://equipo1.prog5a.com/assets/img/pizzas/1/principal.png" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
@@ -132,7 +132,7 @@
                 </div>
             </div>
             <div class="card">
-                <img class="card-img-top" src="assets/img/pizzas/1/principal.png" alt="Card image cap">
+                <img class="card-img-top" src="https://equipo1.prog5a.com/assets/img/pizzas/1/principal.png" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
@@ -146,7 +146,7 @@
         <div class="row justify-content-center">
             <div class="col-lg-3 text-center text-lg-start d-flex flex-column align-items-center order-first align-items-lg-start order-lg-last item social">
                 <div class="fw-bold d-flex align-items-center mb-2"><span class="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center me-2 bs-icon">
-                        <img src="assets\img\Nerd-Pizza.png" alt="Nerd Pizza" id="LogoNerdPizza">
+                        <img src="https://equipo1.prog5a.com/assets\img\Nerd-Pizza.png" alt="Nerd Pizza" id="LogoNerdPizza">
                     </span>
                     <path fill-rule="evenodd" d="M0 10.5A1.5 1.5 0 0 1 1.5 9h1A1.5 1.5 0 0 1 4 10.5v1A1.5 1.5 0 0 1 2.5 13h-1A1.5 1.5 0 0 1 0 11.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm10.5.5A1.5 1.5 0 0 1 13.5 9h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM6 4.5A1.5 1.5 0 0 1 7.5 3h1A1.5 1.5 0 0 1 10 4.5v1A1.5 1.5 0 0 1 8.5 7h-1A1.5 1.5 0 0 1 6 5.5v-1zM7.5 4a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z">
                     </path>
@@ -183,21 +183,21 @@
         </div>
         <div id="RedesSociales" align="center">
             <hr id="AntesSociales">
-            <script lang="JavaScript" src="assets\js\Redes Sociales.js"></script>
+            <script lang="JavaScript" src="https://equipo1.prog5a.com/assets\js\Redes Sociales.js"></script>
             <h3><a href="mailto:correo@server.com" class="TítuloArtículo" id="ContactoCorreo">Contacto</a></h3>
-            <img class="RedSocial" alt="Ícono de Facebook" id="Facebook" onclick="javascript:AbrirFacebook();" src="assets/img/íconoFacebook.png" />
-            <img class="RedSocial" alt="Ícono de WhatsApp" id="WhatsApp" onclick="javascript:AbrirWhatsApp();" src="assets/img/íconoWhatsApp.png" />
-            <img class="RedSocial" alt="Ícono de Messenger" id="Messenger" onclick="javascript:AbrirMessenger();" src="assets/img/íconoMessenger.png" />
-            <img class="RedSocial" alt="Ícono de Telegram" id="Telegram" onclick="javascript:AbrirTelegram();" src="assets/img/íconoTelegram.png" />
-            <img class="RedSocial" alt="Ícono de Twitter" id="Twitter" onclick="javascript:AbrirTwitter();" src="assets/img/íconoTwitter.png" />
-            <img class="RedSocial" alt="Ícono de Instagram" id="Instagram" onclick="javascript:AbrirInstagram();" src="assets/img/íconoInstagram.png" />
+            <img class="RedSocial" alt="Ícono de Facebook" id="Facebook" onclick="javascript:AbrirFacebook();" src="https://equipo1.prog5a.com/assets/img/íconoFacebook.png" />
+            <img class="RedSocial" alt="Ícono de WhatsApp" id="WhatsApp" onclick="javascript:AbrirWhatsApp();" src="https://equipo1.prog5a.com/assets/img/íconoWhatsApp.png" />
+            <img class="RedSocial" alt="Ícono de Messenger" id="Messenger" onclick="javascript:AbrirMessenger();" src="https://equipo1.prog5a.com/assets/img/íconoMessenger.png" />
+            <img class="RedSocial" alt="Ícono de Telegram" id="Telegram" onclick="javascript:AbrirTelegram();" src="https://equipo1.prog5a.com/assets/img/íconoTelegram.png" />
+            <img class="RedSocial" alt="Ícono de Twitter" id="Twitter" onclick="javascript:AbrirTwitter();" src="https://equipo1.prog5a.com/assets/img/íconoTwitter.png" />
+            <img class="RedSocial" alt="Ícono de Instagram" id="Instagram" onclick="javascript:AbrirInstagram();" src="https://equipo1.prog5a.com/assets/img/íconoInstagram.png" />
         </div>
         <div class="text-center text-sm-center text-md-center text-lg-center text-xl-center text-xxl-center d-flex justify-content-center align-items-center align-content-center justify-content-md-center align-items-md-center pt-3">
             <p class="text-center text-muted d-flex d-md-flex align-items-center justify-content-md-center align-items-md-center mb-0">
                 Copyright © Nerd Pizza 2022</p>
         </div>
     </div>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://equipo1.prog5a.com/assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
 
 </html>
