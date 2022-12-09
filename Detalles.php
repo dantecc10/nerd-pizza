10df23d41ab3bdc63e5e79fbdf6320072ca4f928
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-12-09 09:53:32 -0600

    columna

--- Detalles.php
+++ Detalles.php
@@ -6,5 +6,5 @@ include "php scripts/Conexión.php";
 
 $consulta = "SELECT COUNT(*) FROM `$categoría`";
 $cantidad = mysqli_query($conexión, $consulta) or die("Error en la consulta a la base de datos");
-echo $cantidad;
+echo $columna;
 $resultado = mysqli_query($conexión, $consulta) or die("Error en la consulta a la base de datos");
