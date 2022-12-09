aba97a47887c2cf6bdcc710c637fd6d87fc6885c
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-12-09 09:54:21 -0600

    a ver

--- Detalles.php
+++ Detalles.php
@@ -4,7 +4,7 @@ $id = $_GET['id'];
 
 include "php scripts/Conexión.php";
 
-$consulta = "SELECT COUNT(*) FROM `$categoría`";
+$consulta = "SELECT COUNT(*) FROM `$categoría` AS total";
 $cantidad = mysqli_query($conexión, $consulta) or die("Error en la consulta a la base de datos");
-echo $columna;
+echo $columna['total'];
 $resultado = mysqli_query($conexión, $consulta) or die("Error en la consulta a la base de datos");
