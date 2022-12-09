e59eafce5d23c02402bf31d1662e65fbd45f639a
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-12-09 10:02:19 -0600

    a ver

--- Detalles.php
+++ Detalles.php
@@ -1,10 +1,23 @@
 <?php
-$categoría = $_GET['categoria'];
+$categoría = $_GET['categoría'];
 $id = $_GET['id'];
+
+switch ($categoría) {
+    case 'pizzas':
+        $idConsulta = "idPizza";
+        break;
+    case 'complementos':
+        $idConsulta = "idComplementof";
+        break;
+    case 'bebidas':
+        $idConsulta = "idBebidas";
+        break;
+    default:
+        echo "Hubo un error";
+        break;
+}
 
 include "php scripts/Conexión.php";
 
-$consulta = "SELECT COUNT(*) FROM `$categoría` AS total";
-$cantidad = mysqli_query($conexión, $consulta) or die("Error en la consulta a la base de datos");
-echo $columna['total'];
+$consulta = "SELECT * FROM `$categoría` WHERE ";
 $resultado = mysqli_query($conexión, $consulta) or die("Error en la consulta a la base de datos");
