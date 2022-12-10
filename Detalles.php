64c51f8976f6d248ba5fb67499aff27eff325ac7
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-12-09 18:03:17 -0600

    echo de consulta

--- Detalles.php
+++ Detalles.php
@@ -20,6 +20,7 @@ switch ($categoría) {
 include "php scripts/Conexión.php";
 
 $consulta = "SELECT * FROM `$categoría` WHERE `$idConsulta` = `$id`";
+echo $consulta;
 $resultado = mysqli_query($conexión, $consulta) or die("Error en la consulta a la base de datos");
 
 #while ($columna = mysqli_fetch_array($resultado)) {
